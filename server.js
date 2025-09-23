09f5e2bdef5d8ab3e2c9e3bce782e9620b928380
Ahmed Maher <ahmedmaher.dev3@gmail.com>
2025-09-23 15:52:23 +0300

    User New Ahmed

---
+++ server.js
@@ -0,0 +1,26 @@
+/*
+أولًا: العمليات الحسابية الأساسية (Arithmetic Operators)
+
++ الجمع
+
+- الطرح
+
+* الضرب
+
+/ القسمة
+
+% باقي القسمة (modulus)
+
+** الأس (exponentiation)
+
+++ زيادة 1
+
+-- إنقاص 1
+*/
+
+let a = [1, 2, 3 , `A`, `S`, `G`,7,10];
+
+if (Array.isArray(a)) {
+}console.log(`This is an array ${a[3]}`);
+
+
